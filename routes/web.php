2ddd992b3d7795045e48a0fdd96970b1924b6fec
mejorqui <40298919+mejorqui@users.noreply.github.com>
2021-07-26 20:20:19 -0400

    comenzado el curd de paciente

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('tratamiento',\App\Http\Controllers\TratamientoController::class)->middleware('auth');
+Route::resource('paciente',\App\Http\Controllers\PacienteController::class)->middleware('auth');
